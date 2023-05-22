5b7916ceaed9091f025f2ab5d1da6edc5f81a26a
KarenErosa <kerosas10@gmail.com>
2023-05-22 11:26:24 -0600

    Actualización de base de datos

--- BaseDeDatos/Llenado_tablas.sql
+++ BaseDeDatos/Llenado_tablas.sql
@@ -16,161 +16,143 @@ INSERT INTO categorias (nombre) VALUES
     ('Haz tu desmadre'),
     ('Extras');
 
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (2, 'PAPAS A LA FRANCESA', 52.00, 0),
-    (2, 'QUESANIZADAS', 72.00, 0),
-    (2, 'NACHOS', 82.00, 0),
-    (2, 'TAQUITOS DE RES', 72.00, 0),
-    (2, 'DEDOS DE QUESO', 78.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (3, 'PANINI DE POLLO', 85.00, 0),
-    (3, 'PANINI DE ATUN', 82.00, 0),
-    (3, 'PANINI DE PIZZA', 85.00, 0),
-    (3, 'PANINI DE CARNES', 98.00, 0),
-    (3, 'PANINI DE MEXICANO', 88.00, 0),
-    (3, 'PANINI DE CLASICO', 72.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (4, 'CREPA COOKIES AND CREAM', 68.00, 0),
-    (4, 'CREPA NUTELLA FRESA', 78.00, 0),
-    (4, 'CREPA NUTELLA PLATANO', 56.00, 0),
-    (4, 'CREPA CAJETA PLATANO', 56.00, 0),
-    (4, 'CREPA FRESAS CON CREMA', 82.00, 0),
-    (4, 'CREPA MANGO DULCE', 72.00, 0),
-    (4, 'CREPA MORAS CON CHOCOLATE', 78.00, 0),
-    (4, 'CREPA EXPLO. DE CHOCOLATE', 75.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (5, 'CREPA PIZZA', 78.00, 0),
-    (5, 'CREPA ATUN A LA MEXICANA', 68.00, 0),
-    (5, 'CREPA LASAÑA', 82.00, 0),
-    (5, 'CREPA SUIZA', 82.00, 0),
-    (5, 'CREPA POBLANA', 88.00, 0),
-    (5, 'CREPA SICRONIZADA', 83.00, 0),
-    (5, 'CREPA MOLE VERDE', 95.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (6, 'ENSALADA DE FRUTAS', 92.00, 0),
-    (6, 'ENSALADA DE POLLO', 85.00, 0),
-    (6, 'ENSALADA DE ATUN', 85.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (7, 'GALLETAS DE CHISPAS', 12.00, 0),
-    (7, 'GALLETAS DE RED VELVET', 12.00, 0),
-    (7, 'BROWNIE CON HELADO', 52.00, 0),
-    (7, 'CHEESCAKE DEL DIA', 46.00, 0),
-    (7, 'PASTEL DE CHOCOLATE', 50.00, 0),
-    (7, 'PAY DE LIMON', 40.00, 0),
-    (7, 'HELADO', 35.00, 0),
-    (7, 'FRESAS CON CREMA', 50.00, 0),
-    (7, 'PANQUE DE NARANJA', 32.00, 0),
-    (7, 'BANANA SPLIT', 72.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (8, 'FRAPPUCCINO', 46.00, 0),
-    (8, 'FRAPPUCCINO MOKA OSCURO', 52.00, 0),
-    (8, 'FRAPPUCCINO MOKA BLANCO', 52.00, 0),
-    (8, 'FRAPPUCCINO CARAMELO', 52.00, 0),
-    (8, 'FRAPPUCCINO OREO', 52.00, 0),
-    (8, 'FRAPPUCCINO VAINILLA', 52.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (9, 'FRAPPE DE CHOCOLATE', 46.00, 0),
-    (9, 'FRAPPE DE CHOMENTA', 54.00, 0),
-    (9, 'FRAPPE SELVA NEGRA', 62.00, 0),
-    (9, 'FRAPPE MAZAPAN', 58.00, 0),
-    (9, 'MAGNUM', 72.00, 0),
-    (9, 'KINDER', 68.00, 0),
-    (9, 'FRAPPE FERRERO', 52.00, 0),
-    (9, 'FRAPPE YAKULT', 68.00, 0),
-    (9, 'FRAPPE RAFAELLO', 56.00, 0),
-    (9, 'FRAPPE PANDITAS', 45.00, 0),
-    (9, 'FRAPPE GANSITO', 62.00, 0),
-    (9, 'FRAPPE PAY DE LIMON', 52.00, 0),
-    (9, 'FRAPPE DE UNICORNIO', 68.00, 0),
-    (9, 'FRAPPE FRESAS CON CREMA', 60.00, 0),
-    (9, 'FRAPPE CHOCOFRESA', 62.00, 0),
-    (9, 'FRAPPE CHOCOCEREZA', 62.00, 0),
-    (9, 'FRAPPE PIÑA COLADA', 62.00, 0),
-    (9, 'FRAPPE DE MORAS CON AGUA', 48.00, 0),
-    (9, 'FRAPPE DE MORAS CON LECHE', 52.00, 0),
-    (9, 'FRAPPE MANGO CON CHILE', 52.00, 0),
-    (9, 'FRAPPE MANGO CON LECHE', 52.00, 0),
-    (9, 'FRAPPE MANGO', 46.00, 0),
-    (9, 'FRAPPE ACIDITO DE CEREZA', 52.00, 0),
-    (9, 'FRAPPE SUNRISE', 46.00, 0),
-    (9, 'FRAPPE PICAFRESA', 68.00, 0),
-    (9, 'FRAPPE SKWINCLES', 58.00, 0),
-    (9, 'FRAPPE PELON', 52.00, 0),
-    (9, 'FRAPPE PULPARINDO', 58.00, 0),
-    (9, 'FRAPPE MATCHA', 62.00, 0),
-    (9, 'FRAPPE CHAI', 58.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (10, 'Malteada de Chocolate', 52.00, 0),
-    (10, 'Malteada de Vainilla', 52.00, 0),
-    (10, 'Malteada de Fresa', 52.00, 0),
-    (10, 'Malteada de Ferrero', 55.00, 0),
-    (10, 'Malteada de Oreo', 55.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (11, 'TE HELADO', 32.00, 0),
-    (11, 'BUBBLE SODA', 58.00, 0),
-    (11, 'SODA ITALIANA', 58.00, 0),
-    (11, 'LIMONADA NATURAL', 40.00, 0),
-    (11, 'LIMONADA MINERAL', 40.00, 0),
-    (11, 'NARANJADA NATURAL', 40.00, 0),
-    (11, 'NARANJADA MINERAL', 40.00, 0),
-    (11, 'LECHE FRIA', 20.00, 0),
-    (11, 'AGUA NATURAL', 20.00, 0),
-    (11, 'AGUA MINERAL', 22.00, 0),
-    (11, 'REFRESCO', 22.00, 0),
-    (11, 'JUGO DE NARANJA', 30.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (12, 'AMERICANO', 35.00, 0),
-    (12, 'AMERICANO CORTADO', 45.00, 0),
-    (12, 'DESCAFEINADO', 35.00, 0),
-    (12, 'CAPUCCINO', 46.00, 0),
-    (12, 'LATTE', 46.00, 0),
-    (12, 'CAPUCCINO VAINILLA', 48.00, 0),
-    (12, 'SABOR EXTRA JARABE', 14.00, 0),
-    (12, 'CHOCOLATE CALIENTE', 45.00, 0),
-    (12, 'BOMBA DE CHOCOLATE', 65.00, 0),
-    (12, 'CHOCOLATE CON MENTA', 50.00, 0),
-    (12, 'CHOCOLATE SELVA NEGRA', 60.00, 0),
-    (12, 'CHOCOLATE RAFAELLO', 56.00, 0),
-    (12, 'MOKA BLANCO', 50.00, 0),
-    (12, 'MOKA OSCURO', 50.00, 0),
-    (12, 'EXPRESSO', 30.00, 0),
-    (12, 'EXPRESSO CORTADO', 35.00, 0),
-    (12, 'AFFOGATO', 46.00, 0),
-    (12, 'TE CHAI', 56.00, 0),
-    (12, 'TE', 25.00, 0),
-    (12, 'MATCHA', 56.00, 0),
-    (12, 'LECHE CALIENTE', 20.00, 0),
-    (12, 'TIZANAS', 50.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (13, 'EXPRESSO', 30.00, 0),
-    (13, 'EXPRESSO CORTADO', 35.00, 0),
-    (13, 'AFFOGATO', 46.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (14, 'TIZANA DE FRUTOS ROJOS', 58.00, 0),
-    (14, 'TIZANA DE MORA AZUL', 58.00, 0),
-    (14, 'TIZANA DE MANZANA VERDE', 58.00, 0),
-    (14, 'TIZANA DE MANGO LIMON', 58.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (15, 'PANINI 1 INGREDIENTE', 52.00, 0),
-    (15, 'PANINI 2 INGREDIENTES', 68.00, 0),
-    (15, 'PANINI 3 INGREDIENTES', 84.00, 0),
-    (15, 'CREPA 1 INGREDIENTE', 48.00, 0),
-    (15, 'CREPA 2 INGREDIENTES', 64.00, 0),
-    (15, 'CREPA 3 INGREDIENTES', 78.00, 0),
-    (15, 'INGREDIENTE EXTRA', 18.00, 0);
-INSERT INTO productos (categoria_id, nombre, precio, descuento) VALUES
-    (16, 'SABOR EXTRA JARABE', 14.00, 0);
-
-INSERT INTO mesas (nombre_mesa, ocupada, comensales, empleado_id) VALUES
-    ('E1', 0, 0, null),
-    ('E2', 0, 0, null),
-    ('S1', 0, 0, null),
-    ('S2', 0, 0, null),
-    ('S3', 0, 0, null),
-    ('S4', 0, 0, null),
-    ('S5', 0, 0, null),
-    ('S6', 0, 0, null),
-    ('S1', 0, 0, null),
-    ('J1', 0, 0, null),
-    ('J2', 0, 0, null),
-    ('J3', 0, 0, null);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (2, 'PAPAS A LA FRANCESA', 52.00),
+    (2, 'QUESANIZADAS', 72.00),
+    (2, 'NACHOS', 82.00),
+    (2, 'TAQUITOS DE RES', 72.00),
+    (2, 'DEDOS DE QUESO', 78.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (3, 'PANINI DE POLLO', 85.00),
+    (3, 'PANINI DE ATUN', 82.00),
+    (3, 'PANINI DE PIZZA', 85.00),
+    (3, 'PANINI DE CARNES', 98.00),
+    (3, 'PANINI DE MEXICANO', 88.00),
+    (3, 'PANINI DE CLASICO', 72.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (4, 'CREPA COOKIES AND CREAM', 68.00),
+    (4, 'CREPA NUTELLA FRESA', 78.00),
+    (4, 'CREPA NUTELLA PLATANO', 56.00),
+    (4, 'CREPA CAJETA PLATANO', 56.00),
+    (4, 'CREPA FRESAS CON CREMA', 82.00),
+    (4, 'CREPA MANGO DULCE', 72.00),
+    (4, 'CREPA MORAS CON CHOCOLATE', 78.00),
+    (4, 'CREPA EXPLO. DE CHOCOLATE', 75.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (5, 'CREPA PIZZA', 78.00),
+    (5, 'CREPA ATUN A LA MEXICANA', 68.00),
+    (5, 'CREPA LASAÑA', 82.00),
+    (5, 'CREPA SUIZA', 82.00),
+    (5, 'CREPA POBLANA', 88.00),
+    (5, 'CREPA SICRONIZADA', 83.00),
+    (5, 'CREPA MOLE VERDE', 95.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (6, 'ENSALADA DE FRUTAS', 92.00),
+    (6, 'ENSALADA DE POLLO', 85.00),
+    (6, 'ENSALADA DE ATUN', 85.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (7, 'GALLETAS DE CHISPAS', 12.00),
+    (7, 'GALLETAS DE RED VELVET', 12.00),
+    (7, 'BROWNIE CON HELADO', 52.00),
+    (7, 'CHEESCAKE DEL DIA', 46.00),
+    (7, 'PASTEL DE CHOCOLATE', 50.00),
+    (7, 'PAY DE LIMON', 40.00),
+    (7, 'HELADO', 35.00),
+    (7, 'FRESAS CON CREMA', 50.00),
+    (7, 'PANQUE DE NARANJA', 32.00),
+    (7, 'BANANA SPLIT', 72.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (8, 'FRAPPUCCINO', 46.00),
+    (8, 'FRAPPUCCINO MOKA OSCURO', 52.00),
+    (8, 'FRAPPUCCINO MOKA BLANCO', 52.00),
+    (8, 'FRAPPUCCINO CARAMELO', 52.00),
+    (8, 'FRAPPUCCINO OREO', 52.00),
+    (8, 'FRAPPUCCINO VAINILLA', 52.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (9, 'FRAPPE DE CHOCOLATE', 46.00),
+    (9, 'FRAPPE DE CHOMENTA', 54.00),
+    (9, 'FRAPPE SELVA NEGRA', 62.00),
+    (9, 'FRAPPE MAZAPAN', 58.00),
+    (9, 'MAGNUM', 72.00),
+    (9, 'KINDER', 68.00),
+    (9, 'FRAPPE FERRERO', 52.00),
+    (9, 'FRAPPE YAKULT', 68.00),
+    (9, 'FRAPPE RAFAELLO', 56.00),
+    (9, 'FRAPPE PANDITAS', 45.00),
+    (9, 'FRAPPE GANSITO', 62.00),
+    (9, 'FRAPPE PAY DE LIMON', 52.00),
+    (9, 'FRAPPE DE UNICORNIO', 68.00),
+    (9, 'FRAPPE FRESAS CON CREMA', 60.00),
+    (9, 'FRAPPE CHOCOFRESA', 62.00),
+    (9, 'FRAPPE CHOCOCEREZA', 62.00),
+    (9, 'FRAPPE PIÑA COLADA', 62.00),
+    (9, 'FRAPPE DE MORAS CON AGUA', 48.00),
+    (9, 'FRAPPE DE MORAS CON LECHE', 52.00),
+    (9, 'FRAPPE MANGO CON CHILE', 52.00),
+    (9, 'FRAPPE MANGO CON LECHE', 52.00),
+    (9, 'FRAPPE MANGO', 46.00),
+    (9, 'FRAPPE ACIDITO DE CEREZA', 52.00),
+    (9, 'FRAPPE SUNRISE', 46.00),
+    (9, 'FRAPPE PICAFRESA', 68.00),
+    (9, 'FRAPPE SKWINCLES', 58.00),
+    (9, 'FRAPPE PELON', 52.00),
+    (9, 'FRAPPE PULPARINDO', 58.00),
+    (9, 'FRAPPE MATCHA', 62.00),
+    (9, 'FRAPPE CHAI', 58.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (10, 'Malteada de Chocolate', 52.00),
+    (10, 'Malteada de Vainilla', 52.00),
+    (10, 'Malteada de Fresa', 52.00),
+    (10, 'Malteada de Ferrero', 55.00),
+    (10, 'Malteada de Oreo', 55.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (11, 'TE HELADO', 32.00),
+    (11, 'BUBBLE SODA', 58.00),
+    (11, 'SODA ITALIANA', 58.00),
+    (11, 'LIMONADA NATURAL', 40.00),
+    (11, 'LIMONADA MINERAL', 40.00),
+    (11, 'NARANJADA NATURAL', 40.00),
+    (11, 'NARANJADA MINERAL', 40.00),
+    (11, 'LECHE FRIA', 20.00),
+    (11, 'AGUA NATURAL', 20.00),
+    (11, 'AGUA MINERAL', 22.00),
+    (11, 'REFRESCO', 22.00),
+    (11, 'JUGO DE NARANJA', 30.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (12, 'AMERICANO', 35.00),
+    (12, 'AMERICANO CORTADO', 45.00),
+    (12, 'DESCAFEINADO', 35.00),
+    (12, 'CAPUCCINO', 46.00),
+    (12, 'LATTE', 46.00),
+    (12, 'CAPUCCINO VAINILLA', 48.00),
+    (12, 'CHOCOLATE CALIENTE', 45.00),
+    (12, 'BOMBA DE CHOCOLATE', 65.00),
+    (12, 'CHOCOLATE CON MENTA', 50.00),
+    (12, 'CHOCOLATE SELVA NEGRA', 60.00),
+    (12, 'CHOCOLATE RAFAELLO', 56.00),
+    (12, 'MOKA BLANCO', 50.00),
+    (12, 'MOKA OSCURO', 50.00),
+    (12, 'TE CHAI', 56.00),
+    (12, 'TE', 25.00),
+    (12, 'MATCHA', 56.00),
+    (12, 'LECHE CALIENTE', 20.00),
+    (12, 'TIZANAS', 50.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (13, 'EXPRESSO', 30.00),
+    (13, 'EXPRESSO CORTADO', 35.00),
+    (13, 'AFFOGATO', 46.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (14, 'TIZANA DE FRUTOS ROJOS', 58.00),
+    (14, 'TIZANA DE MORA AZUL', 58.00),
+    (14, 'TIZANA DE MANZANA VERDE', 58.00),
+    (14, 'TIZANA DE MANGO LIMON', 58.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (15, 'PANINI 1 INGREDIENTE', 52.00),
+    (15, 'PANINI 2 INGREDIENTES', 68.00),
+    (15, 'PANINI 3 INGREDIENTES', 84.00),
+    (15, 'CREPA 1 INGREDIENTE', 48.00),
+    (15, 'CREPA 2 INGREDIENTES', 64.00),
+    (15, 'CREPA 3 INGREDIENTES', 78.00),
+    (15, 'INGREDIENTE EXTRA', 18.00);
+INSERT INTO productos (categoria_id, nombre, precio) VALUES
+    (16, 'SABOR EXTRA JARABE', 14.00);
